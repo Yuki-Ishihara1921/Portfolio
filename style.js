6ddd8445b9d6c857d21794439a4ba00be3366ab5
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-03-12 18:17:17 +0900

    remaked portfolio

--- style.js
+++ style.js
@@ -6,7 +6,7 @@ $(function() {
     })
 
     $("a[href^='#']").click(function() {
-        var speed = 500
+        var speed = 50
         var href = $(this).attr("href")
         var target = $(href == "#" || href == "" ? 'html' : href)
         var position = target.offset().top
