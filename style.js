f582057f14c51b0887e919fb94ba3895ff61d1f3
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-03-13 16:46:07 +0900

    modify 'var' into 'const'

--- style.js
+++ style.js
@@ -6,11 +6,11 @@ $(function() {
     })
 
     $("a[href^='#']").click(function() {
-        var speed = 50
-        var href = $(this).attr("href")
-        var target = $(href == "#" || href == "" ? 'html' : href)
-        var position = target.offset().top
-        $('body, html').animate({scrollTop:position}, speed, "swing")
+        const speed = 50
+        const href = $(this).attr("href")
+        const target = $(href == "#" || href == "" ? 'html' : href)
+        const position = target.offset().top
+        $('body, html').animate({ scrollTop: position }, speed, "swing")
         if($('div').hasClass('toggled')) {
             $("#menu-toggle > i.fa-bars, #menu-toggle > i.fa-times").toggleClass("fa-bars fa-times")
             $("div").removeClass("toggled")
